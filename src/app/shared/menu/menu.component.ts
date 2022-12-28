024f51f5501a2f3d1e19703a300e845c6551704c
Jose Alfredo <joseafedox@gmail.com>
2022-12-27 21:50:10 -0600

    generating the menu components

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -6,34 +7,40 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
   items:MenuItem[]=[];
-  ngOnInit() {
+
+  ngOnInit():void {
     this.items = [
-        {
-            label: 'File',
-            items: [{
-                    label: 'New', 
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        {label: 'Project'},
-                        {label: 'Other'},
-                    ]
-                },
-                {label: 'Open'},
-                {label: 'Quit'}
-            ]
-        },
-        {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-                {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-            ]
-        }
-    ];
-}
+      {
+        label:'pipes de Angular',
+        icon: 'pi pi-desktop',
+        items:[
+          {
+            label: 'Textos y Fechas',
+            icon:'pi pi-align-left',
+            routerLink:'/'
+          },
+          {
+            label: 'Numeros',
+            icon:'pi pi-dollar',
+            routerLink:'numeros'
+          }
+          ,
+          {
+            label: 'No Comunes',
+            icon:'pi pi-globe',
+            routerLink:'no-comunes'
+          }
+        ]
+      },
+      {
+        label:'Pipes Perzonalizados',
+        icon:'pi pi-cog'
+      }
+  ];
+  }
+  
   
 }

cdb304528d512458d948634050f0973b16f0738d
Jose Alfredo <joseafedox@gmail.com>
2022-12-27 21:24:51 -0600

    create routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -12,7 +13,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PipesComponent } from './pipes/pipes.component';
-import { PagesComponent } from './pages/pages.component';
+import { NumerosComponent } from './pages/numeros/numeros.component';
+import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
+import { BasicosComponent } from './pages/basicos/basicos.component';
+import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
 
 
 @NgModule({
   declarations: [
     PipesComponent,
-    PagesComponent
+    NumerosComponent,
+    NoComunesComponent,
+    BasicosComponent,
+    OrdenarComponent
+  ],
+  exports:[
+    NumerosComponent,
+    NoComunesComponent,
+    BasicosComponent,
+    OrdenarComponent
   ],
   imports: [
     CommonModule
